6604ccbc635ff60c77658588b88063d0fcb12863
Patrick Denert <pdenert@gmail.com>
2023-08-14 14:02:28 -0400

    backend

---
+++ routes/record.mjs
@@ -0,0 +1,71 @@
+import express from "express";
+import db from "../db/conn.mjs";
+import { ObjectId } from "mongodb";
+
+const router = express.Router();
+
+// This section will help you get a list of all the records.
+router.get("/", async (req, res) => {
+  let collection = await db.collection("recipes");
+  let results = await collection.find({}).toArray();
+  res.send(results).status(200);
+});
+
+// This section will help you get a single record by id
+router.get("/:id", async (req, res) => {
+  let collection = await db.collection("recipes");
+  let query = {_id: new ObjectId(req.params.id)};
+
+  let result = await collection.findOne(query);
+
+  if (!result) res.send("Not found").status(404);
+  else res.send(result).status(200);
+});
+
+// This section will help you create a new record.
+router.post("/", async (req, res) => {
+  let newRecipe = {
+    name: req.body.name,
+    servings: req.body.servings,
+    time: req.body.time,
+    rating: req.body.rating,
+    base64Img: req.body.base64Img,
+    ingredients: req.body.ingredients,
+    instructions: req.body.instructions,
+  };
+
+  let collection = await db.collection("recipes");
+  let result = await collection.insertOne(newRecipe);
+  console.log(newRecipe);
+  res.send(result).status(204);
+});
+
+// This section will help you update a record by id.
+router.patch("/:id", async (req, res) => {
+  const query = { _id: new ObjectId(req.params.id) };
+  const updates =  {
+    $set: {
+      name: req.body.name,
+      servings: req.body.servings,
+      time: req.body.time,
+      rating: req.body.rating,
+    }
+  };
+
+  let collection = await db.collection("recipes");
+  let result = await collection.updateOne(query, updates);
+
+  res.send(result).status(200);
+});
+
+// This section will help you delete a record
+router.delete("/:id", async (req, res) => {
+  const query = { _id: new ObjectId(req.params.id) };
+
+  const collection = db.collection("recipes");
+  let result = await collection.deleteOne(query);
+
+  res.send(result).status(200);
+});
+
+export default router;
